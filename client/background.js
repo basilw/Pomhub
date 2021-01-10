91b94acd13a5fa568b17234b7da19b986c2e7239
gavin2059 <gavin2600@gmail.com>
2021-01-10 12:16:54 +0800

    updated sending request info to background.js

--- client/background.js
+++ client/background.js
@@ -1,21 +1,27 @@
 const socket = io('http://localhost');
-socket.on('connect', function() {
-  socket.emit('connect', 'namehere', 'room');
- });
-// Called when a new page is loaded
-chrome.webNavigation.onDOMContentLoaded.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var url = tabs[0].url;
-    socket.emit('pageload','namehere', 'room', url);
-  });
-});
+var nameID;
+var roomID;
 
 // Listen to messages
 chrome.runtime.onMessage.addListener(
   function(request) {
     if( request.message === "join" ) {
-      
+      socket.connect();
+      nameID = request.nameID;
+      roomID = request.roomID;
     }
   }
 );
+
+socket.on('connect', function() {
+  socket.emit('connect', nameID, roomID);
+ });
+
+// Called when a new page is loaded
+chrome.webNavigation.onDOMContentLoaded.addListener(function(tab) {
+// Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var url = tabs[0].url;
+    socket.emit('pageload', nameID, roomID, url);
+  });
+});
